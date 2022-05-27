975412113002a060ff28bd2a0528c184c8addcf6
Gabriel Guerra <inlive04@outlook.com>
2022-05-27 11:51:26 -0300

    Estudo JavaScript 27-05-2022:11:50

---
+++ JavaScript/Aula11/CondicionalJS.js
@@ -0,0 +1,11 @@
+var vel = 59
+console.log(`A velocidade do viuculo é de : ${vel}KM Por Hora.`)
+if ( vel >= 60)
+    {
+        console.log('Voce Foi Multado ! Excesso de Velocidade !')
+    }
+else
+    {
+        console.log('Velocidade Dentro do permitido.')
+    }
+console.log('Dirija sempre usando os cintos de Segurança.')
