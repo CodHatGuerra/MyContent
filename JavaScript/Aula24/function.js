51c76d43132c3be3502598fe1ac139e9784adbd2
Gabriel Guerra <inlive04@outlook.com>
2022-09-27 17:34:27 -0300

    Subindo Estudo Bootstrap 27-09-2022 17:34

--- JavaScript/Aula24/function.js
+++ JavaScript/Aula24/function.js
@@ -11,3 +11,24 @@ var numeros = [1,2,3,4,5]
 
 var total = somarray(numeros)
 console.log(total)
+
+function addLeadingZeros(num, totalLength) {
+    return String(num).padStart(totalLength, '0');
+  }
+
+  console.log(addLeadingZeros(3, 2));
+
+  console.log(moment(new Date(2021, 1, 1)).toISOString())
+
+
+
+var a = "I want apple";
+var b = " an";
+var position = 6;
+var output = [a.slice(0, position), b, a.slice(position)].join('');
+console.log(output);
+
+
+var a = ['a', 'b', 'c']
+
+console.log(a.length)
