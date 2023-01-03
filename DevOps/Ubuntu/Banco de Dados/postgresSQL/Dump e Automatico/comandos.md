993ae0c348b1c7d17c1df16c8a9c8e88c26bb0a8
Gabriel Guerra <inlive04@outlook.com>
2023-01-03 17:47:58 -0300

    ajustes

---
+++ DevOps/Ubuntu/Banco de Dados/postgresSQL/Dump e Automatico/comandos.md
@@ -0,0 +1,5 @@
+### Para executar um Dump de uma unica base de dados execute o comandos abaixo
+#### 1 - `su postgres`
+#### 2 - `pg_dump dbname > dbname.bak`
+#### O Dump sera armazenado no seguinte repositório `/var/lib/postgresql`
+#### Para Realizar um dump completo de todas as bases de dados presente utilize o seguinte comando `pg_dumpall > pg_backup.bak` no qual se encontrara no mesmo repositório 
--- Frontend/Projeto/assets/common/javascript/commun.js
+++ Frontend/Projeto/assets/common/javascript/commun.js
@@ -1,42 +1,8 @@
-
-async function onclick_Login() {
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: `https://inlivehomologacao.ddns.net/api/api/v1/portal/autenticacao/login`,
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({
-            CodigoAplicacao: 1,
-            Usuario: $('#form_User').val(),
-            Senha: $('#form_Pass').val()
-        }),
-        success : (data) => {
-            localStorage.clear();
-            localStorage.setItem('acess_Token', data.Data.AccessToken);
-            localStorage.setItem('data_TokenExpire', moment(new Date()).add(data.Data.Expires, 's').toDate());
-            message_Show("success")
-        },
-        error :  () => (message_Show("error")),
-    });
-}
-
-async function message_Show(state) {
-    if (state === "success") {
-      await Swal.fire({
-        icon: "success",
-        title: "You have logged in successfully",
-        showConfirmButton: false,
-        timer: 1500,
-      }).then((result) => {
-        window.location.href = "merchant/merchant.html";
-    })
-    } else if (state === "error") {
-      await Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "wrong username or password!",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-    }
-  }
+// $.ajaxSetup({
+//     beforeSend: function(xhr) {
+//         xhr.setRequestHeader('KeyApi', 'FCABC9A2-7FDF-463A-99EB-118313CFE3A2');
+//     }
+//   });
+$.ajaxSetup({
+    headers: {'KeyApi' : 'FCABC9A2-7FDF-463A-99EB-118313CFE3A2'}
+  });
