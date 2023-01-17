ae791cc79a5374c0fac82db18cb254927bfb989c
Gabriel Guerra <inlive04@outlook.com>
2023-01-17 18:26:46 -0300

    estudo javascript

--- Frontend/Projeto/assets/common/javascript/common.js
+++
@@ -1,7 +0,0 @@
-"use strict"    
-
-$.ajaxSetup({
-    headers: {'KeyApi' : 'FCABC9A2-7FDF-463A-99EB-118313CFE3A2'}
-  });
-
-const URL = 'https://inlivedesenvolvimento.ddns.net'
--- Frontend/Projeto/assets/common/javascript/commun.js
+++
@@ -1,8 +0,0 @@
-// $.ajaxSetup({
-//     beforeSend: function(xhr) {
-//         xhr.setRequestHeader('KeyApi', 'FCABC9A2-7FDF-463A-99EB-118313CFE3A2');
-//     }
-//   });
-$.ajaxSetup({
-    headers: {'KeyApi' : 'FCABC9A2-7FDF-463A-99EB-118313CFE3A2'}
-  });
