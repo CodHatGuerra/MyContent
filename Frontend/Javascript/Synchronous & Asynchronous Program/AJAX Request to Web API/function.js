7a3e4a1b4569dc3888fc41a57cdd0a8629ed1637
CodHatGuerra <105330787+CodHatGuerra@users.noreply.github.com>
2023-01-24 00:19:05 -0300

    estudo js

--- Frontend/Javascript/Synchronous & Asynchronous Program/AJAX Request to Web API/function.js
+++ Frontend/Javascript/Synchronous & Asynchronous Program/AJAX Request to Web API/function.js
@@ -4,17 +4,23 @@
     let xhr = new XMLHttpRequest();
 
     //2nd step create the request
-    xhr.open('GET', 'https://restcountries.com/v3.1/name/'+countyName, false);
+    xhr.open('GET', 'https://restcountries.com/v3.1/name/'+countyName, true);
 
     //3rd. send the request
     xhr.send();
     
     //Convert The JSON to javascript OBJECT
-    let response = JSON.parse(xhr.responseText)
-    let countryData = response[0];
+    xhr.onload = (e) => {
+        let response = JSON.parse(xhr.responseText)
+        let countryData = response[0];
     console.log(countryData);
 
-    //another way to Destucture the array
-    let [data] = JSON.parse(xhr.responseText)
+        //another way to Destucture the array
+        let [data] = JSON.parse(xhr.responseText)
+    }
+
+
 }
 getCountry('brasil');
+getCountry('usa');
+getCountry('germany');
