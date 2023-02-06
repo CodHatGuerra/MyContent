b3a4059b5bee76784a01f74c59e4f78738b63caa
Gabriel Guerra <inlive04@outlook.com>
2023-02-06 17:30:14 -0300

    estudo javascript

---
+++ Frontend/Javascript/Operators & Data Structures & Strings/Object Literal/function.js
@@ -0,0 +1,18 @@
+let workingDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+
+let shift = {
+    Mon: '10AM - 7PM',
+    Tue: '10AM - 7PM',
+    Wed: '10AM - 7PM',
+    Thu: '10AM - 7PM',
+    Fri: '10AM - 7PM',
+}
+
+let employee = {
+    id: 101,
+    name: 'John',
+    birthYear: 1990,
+    workingDays,
+    shift
+}
+console.log(employee);
