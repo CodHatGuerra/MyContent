5fc14fc5e4954ba59c79d84e0e0ded570dd63005
CodHatGuerra <105330787+CodHatGuerra@users.noreply.github.com>
2023-03-21 23:49:04 -0300

    subindo estudo Node

---
+++ Backend/NodeJs/Curso/Aula04 - Módulos/calc.js
@@ -0,0 +1,11 @@
+let somaFunc = require("./somar");
+let subFunc = require("./subtracao");
+let multiFunc = require("./multiplicacao");
+let divFunc = require("./divisao");
+
+console.log(somaFunc(2,4));
+console.log(subFunc(2,4));
+console.log(multiFunc(2,4));
+console.log(divFunc(2,4));
+
+//https://www.youtube.com/watch?v=QnTCre0HHv8&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=5&ab_channel=VictorLima-GuiadoProgramador
