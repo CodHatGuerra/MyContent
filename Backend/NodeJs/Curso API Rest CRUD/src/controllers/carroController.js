505dc7af61ff17d2ec782d244e9c85e7bfd02be0
CodHatGuerra <105330787+CodHatGuerra@users.noreply.github.com>
2023-04-06 16:57:53 -0300

    Estudos NodeJS

--- Backend/NodeJs/Curso API Rest CRUD/src/controllers/carroController.js
+++ Backend/NodeJs/Curso API Rest CRUD/src/controllers/carroController.js
@@ -15,4 +15,41 @@ module.exports = {
     }
     res.json(json);
   },
+
+  buscarUm: async (req, res) => {
+    let json = { error: "", result: {} };
+
+    let codigo = req.params.codigo;
+
+    let carro = await CarroService.buscarUm(codigo);
+
+    if (carro) {
+      json.result = {
+        codigo: carro.codigo,
+        modelo: carro.modelo,
+        placa: carro.placa,
+      };
+    }
+
+    res.json(json);
+  },
+
+  inserir: async (req, res) => {
+    let json = { error: "", result: {} };
+
+    let modelo = req.body.modelo;
+    let placa = req.body.placa;
+
+    if (modelo && placa) {
+      let CarroCodigo = await CarroService.inserir(modelo, placa);
+      json.result = {
+        codigo: CarroCodigo,
+        modelo,
+        placa,
+      };
+    } else {
+      json.error = 'Campos não Enviados'
+    }
+    res.json(json);
+  },
 };
--- Backend/NodeJs/Curso API Rest CRUD/src/services/carroService.js
+++ Backend/NodeJs/Curso API Rest CRUD/src/services/carroService.js
@@ -14,4 +14,36 @@ module.exports = {
       });
     });
   },
+
+  buscarUm: (codigo) => {
+    return new Promise((aceito, rejeitado) => {
+      db.query("SELECT * FROM carros WHERE codigo = ?",[codigo],(error, results) => {
+          if (error) {
+            rejeitado(error);
+            return;
+          }
+          if (results.length > 0) {
+            aceito(results[0]);
+            return;
+          } else {
+            aceito(false);
+          }
+        }
+      );
+    });
+  },
+
+
+  inserir: (modelo, placa) => {
+    return new Promise((aceito, rejeitado) => {
+      db.query("INSERT INTO carros (modelo, placa) VALUES (?, ?)" , [modelo, placa] , (error, results) => {
+          if (error) {
+            rejeitado(error);
+            return;
+          }
+          aceito(results.insertCodigo);
+        }
+      );
+    });
+  },
 };
